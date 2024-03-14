7bf67a83eea5e4dd8a3696a3cb84171affd8b7ad
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-03-14 17:20:09 +0100

    updated

--- src/components/wrapper/app-route-wrapper/AppProfile.tsx
+++ src/components/wrapper/app-route-wrapper/AppProfile.tsx
@@ -1,12 +1,13 @@
 import './styles.css';
-import profilePix from './../../../assets/profilePix.png'
+// import profilePix from './../../../assets/profilePix.png'
 import { IoHomeSharp} from "react-icons/io5";
 import { GoProject } from "react-icons/go";
+import profImg from './../../../assets/profImg.jpg'
 const AppProfile = () => {
   return (
     <>
     <div className='profileWrapper'>
-        <img src={profilePix} className='profile' alt="" />
+        <img src={profImg} className='profile' alt="" />
       <p className='name'>Md Shimul Hassain</p>
       <p className='email'>mdshimulhassain11@gmail.com</p>
       <button className='mybtn'>My Overview</button>
--- src/pages/App/app-home-page/AppHomePage.tsx
+++ src/pages/App/app-home-page/AppHomePage.tsx
@@ -1,25 +1,66 @@
 
 // 
 
-import { LineGraph, MyBarChart, TopDueProjects,  } from '../..'
+import { LineGraph, MyBarChart, TopDueProjects, } from '../..'
 import './styles.css'
 
 
 const AppHomePage = () => {
   return (
     <>
-    <h1>Dashboard</h1>
-    <div className="container">
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-    </div>
-   <LineGraph/>
-   <div className='Footer'>
-    <TopDueProjects/>
-    <MyBarChart/>
-   </div>
+      <h1>Dashboard</h1>
+      <div className="container">
+        <div className="card">
+          <div className='innerCard'>
+            <div>
+              <p>Total Projects</p>
+              <p>3</p>
+            </div>
+            <div className='percentage'>
+              <p>46%</p>
+            </div>
+          </div>
+
+        </div>
+        <div className="card">
+        <div className='innerCard'>
+            <div>
+              <p>Total Tasks</p>
+              <p>18</p>
+            </div>
+            <div>
+              <p>74%</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+        <div className='innerCard'>
+            <div>
+              <p>Total Expense</p>
+              <p>5</p>
+            </div>
+            <div>
+              <p>14%</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+        <div className='innerCard'>
+            <div>
+              <p>Total Users</p>
+              <p>5</p>
+            </div>
+            <div>
+              <p>14%</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <LineGraph />
+      <div className='Footer'>
+        <TopDueProjects />
+        <MyBarChart />
+      </div>
     </>
   )
 }
